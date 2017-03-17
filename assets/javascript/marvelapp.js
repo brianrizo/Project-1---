3afdcd1c3dfdaadaf02a4f6eb1eb687fe8afec32
markring <markring@live.com>
2017-03-16 21:35:59 -0400

    cPics object created

--- assets/javascript/marvelapp.js
+++ assets/javascript/marvelapp.js
@@ -104,10 +104,31 @@ Ta72711e158159fe6a72fb9e948da54f3: "Invisible Woman",
 T18e2e8543ccb1c9ec1e2ec64ad2e096c: "Beast",
 T0cccbec4df216ab95c458aa1c6745524: "Cyclops",
 Td2d15e5993dde61d502314a7f7a53243: "Kitty Pryde",
-Ta72711e158159fe6a72fb9e948da54f3: "Invisible Woman",
 T0688410c4d38eb916ff5a3df166debdc: "Rogue",
 T39f7e0b54fd2047fbe76f0649c83001c:  "Iron Man"        
 };
+var cPics {
+Tfeca35057041f5317fc13e82d845ecf9: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MzU5ODQ5OF5BMl5BanBnXkFtZTYwODIwODk1._V1_UY317_CR4,0,214,317_AL_.jpg",
+T850ee1427ef09724c4d11e1d0de71ba8: "http://media.gettyimages.com/photos/actor-charlie-cox-attends-the-premiere-of-stardust-at-the-savoy-on-picture-id77242402",
+T8d40cc0238272eb311f8761e353d998c: "https://s-media-cache-ak0.pinimg.com/736x/7e/df/63/7edf63c4f45cd7ec5d4c5d9cc8a4a564.jpg",
+T36ab889c3a75382de799c658e1f4c3ec: "http://www.workingauthor.com/wp-content/uploads/d23-expo-2011-chris-hemsworth-headshot1.jpg",
+Tec3df6a5995d6a1b03841548f5afa542: "http://vignette3.wikia.nocookie.net/gameofthrones/images/6/6d/600full-sophie-turner.jpg/revision/latest?cb=20141123171917&path-prefix=de",
+T957fc5de3a094f0ff856a6899e31990b: "http://cdn3-www.superherohype.com/assets/uploads/2011/09/file_168508_0_ewavengersheadshots1.jpg",
+Td55a589c2d63b43461803e21251bc06d: "http://greginhollywood.com/wordpress/wp-content/uploads/Shawn-Ashmore-AES-084089.jpg",
+T43ce09c7070b1eb442a3abbe2e3fd00d: "https://s-media-cache-ak0.pinimg.com/236x/dc/24/0e/dc240e2dff96e8127bc616c2ba565799.jpg",
+T5165cef7ef95eb3e521421232c4598c1: "https://s-media-cache-ak0.pinimg.com/originals/75/c6/23/75c623389f80a303e818ce4ef42c6150.jpg",
+T257d6a70441c8deedb09e3ca3a57d52d: "http://vipfanauctions.com/wp-content/uploads/Hugh-Jackman-Chappie.jpg",
+Tdb42371a7d45b946ea4c66c4c8c4b504: "https://i.ytimg.com/vi/ttaLyTSmytE/maxresdefault.jpg",
+T035230f753cdd5cf3c6f1f962ac56125: "http://www.famousbirthdays.com/headshots/andrew-garfield-1.jpg",
+T046404a514ea10b270f61fd3d2b56f8d: "https://pmcdeadline2.files.wordpress.com/2014/09/chadwick-boseman.jpg",
+T17da4a2ad5f7122269fc249607b59233: "http://speakerdata2.s3.amazonaws.com/photo/image/840303/benedictcumberbatch_bbs_012.jpg",
+Ta72711e158159fe6a72fb9e948da54f3: "https://s-media-cache-ak0.pinimg.com/564x/36/57/ea/3657eaa26f106dad095442ca52f1c7ba.jpg",
+T18e2e8543ccb1c9ec1e2ec64ad2e096c: "https://s-media-cache-ak0.pinimg.com/736x/53/1a/ec/531aecb6a8acbad3c11efb544bf17486.jpg",
+T0cccbec4df216ab95c458aa1c6745524: "http://cdn02.cdn.justjaredjr.com/wp-content/uploads/pictures/2015/01/tye-brett/tye-sheridan-brett-davern-stanford-prison-experiment-sundance-29.jpg",
+Td2d15e5993dde61d502314a7f7a53243: "https://s-media-cache-ak0.pinimg.com/originals/78/f3/db/78f3db7d0ed730e6de938fb57734f5c5.jpg",
+T0688410c4d38eb916ff5a3df166debdc: "https://s-media-cache-ak0.pinimg.com/originals/bc/26/80/bc26806334968588e24a67b79fccb5ad.jpg",
+T39f7e0b54fd2047fbe76f0649c83001c: "https://i1.wp.com/www.workingauthor.com/wp-content/uploads/d23-expo-2011-robert-downey-jr-headshot.jpg"
+}
 
 
 function Face(imgURL){
